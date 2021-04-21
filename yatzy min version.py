b58522bca804e6e2a05b490cd212925a54067d98
Jessica Pärnänen <jessparn@lycksele.se>
2021-04-21 15:45:26 +0200

    onsdag lektion 21/4

--- yatzy min version.py
+++ yatzy min version.py
@@ -5,38 +5,58 @@
 
 import random
 def vissa_om_kastningen(tärning):
-    print("deta är tärnings siffrorna",end = '')
+    print("ditt slags siffror ",end = '')
     for d in tärning:
         print(str(d) + ' ', end = '')
     print() 
-regler = ("användaren ska starta programet och dåkommer programtet att slå en tärning(slumpar fram ett nummer) och användaren kommer få välja vilka sifror som personen vill kasta om, när personen har valt : ")
-poäng=0
-tärning = []
 
-print(regler)
+poäng = 0
+poäng_lista = []
 
-for d in range(4):
-    tärning.append(random.randint(1,6))
+spel_slut = false
+while not spel_slut:
+
+
+    tärning = []
+
+    for d in range(4):
+        tärning.append(random.randint(1,6))
+        
+    vissa_om_kastningen(tärning)
     
-vissa_om_kastningen(tärning)
-
-#förklara/frågar användaren om vad för siffror man vill kasta om och räknar ihop dem sen.
-
-kassta_om =input("vilken tärning vill du slå om?")
-kassta_om = kassta_om.split()
-for index, ch in enumerate(kassta_om):
-    kassta_om[index] = int(ch) - 1
+    #förklara/frågar användaren om vad för siffror man vill kasta om och räknar ihop dem sen.
     
-for index in kassta_om:
-    tärning[index] = random.randint(1,6)
-
-vissa_om_kastningen(tärning)
-
-numer_till_poäng = int(input("vilket numer vill du ska räknas som poäng? "))
-poäng_list.append(numer_till_poäng)
-for d in tärning:
-    if d == numer_till_poäng:
-        poäng += d
-print("dina poäng" +str(poäng))
-
-
+    kassta_om =input("vilken tärning vill du slå om?")
+    kassta_om = kassta_om.split()
+    
+    for index, ch in enumerate(kassta_om):
+        kassta_om[index] = int(ch) - 1
+        
+    for index in kassta_om:
+        tärning[index] = random.randint(1,6)
+    
+    vissa_om_kastningen(tärning)
+    
+    nummer_giltigt=fel
+    while not nummer_giltigt:
+        nummer_till_poäng = int(input("vilket numer vill du ska räknas som poäng? "))
+        
+        if nummer_till_poäng not in poäng_lista:
+            nummer_giltigt = sant 
+        else: 
+            print("du har reden valt detta nummer: ") 
+            for num in poäng_lista:
+                print (str(num) + ' ', end = '')
+                print ()
+            
+    poäng_list.append(nummer_till_poäng)
+    for d in tärning:
+        if d == nummer_till_poäng:
+            poäng += d
+            
+    print("dina poäng" +str(poäng))
+    
+    if len(poäng_lista) == 6:
+        game_over = corect 
+    
+    
